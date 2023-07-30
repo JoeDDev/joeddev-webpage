d6608a239d45e5738b6cd1511511d995b8f0c0be
JoeDDev <josdarotblau@gmail.com>
2023-07-30 00:54:54 -0600

    adding first page (photo, user data and user alias

--- joeddev-web/src/App.vue
+++ joeddev-web/src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="left-side">
+      <DataUser/>
+    </div>
+    <div class="right-side">
+      <img class="usr_img" src="./components/img/usr_joeddev.jpg" alt="usr_img">
+    </div>
+  </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DataUser from './components/DataUser.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DataUser
   }
 }
 </script>
@@ -21,6 +27,26 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  display: flex;
+  height: 100vh;
+}
+
+.left-side,
+.right-side {
+  flex: 1;
+  height: 100%;
+}
+
+.left-side {
+  background-color: #ffffff; /* Color del lado izquierdo */
+}
+
+.right-side {
+  background-color: #000000; /* Color del lado derecho */
+}
+.usr_img{
+  image-resolution: ;
 }
 </style>
---
+++ joeddev-web/src/components/DataUser.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="user">
+      <div class="prueba">
+          <div class="first_name">
+              <h1 class="heading">{{user_name}}</h1>
+          </div>
+          <div class="last_name">
+              <h1 class="heading">{{last_name}}</h1>
+          </div>
+          <div class="user_alias">
+              <h2 class="heading">@{{user}}</h2>
+          </div>
+          <div class="prueba">
+            <div class="prueba">
+                Hola mundo
+            </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "UserData",
+    props: {
+
+    },
+    data(){
+        return{
+            user_name: "Jose David",
+            last_name: "Rojas Fuentes",
+            user: "joeddev"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .prueba{
+        border-style: dotted;
+    }
+    .user{
+        display: flex;
+        font-family: 'Kanit', sans-serif;
+        font-weight: bold;
+        margin-left: 10%;
+        margin-right: 10%;
+    }
+    .first_name{
+        font-size: 70px;
+        text-align: left;
+        margin-top: 20%;
+        padding-bottom: -10%;
+    }
+    .last_name{
+        font-size: 35px;
+        text-align: left;
+        padding-top: 12px;
+        padding: 0;
+    }
+    .user_alias{
+        font-size: 30px;
+        text-align: left;
+        padding-top: 12px;
+        padding: 0;
+    }
+    .heading{
+        margin: 0;
+    }
+</style>
